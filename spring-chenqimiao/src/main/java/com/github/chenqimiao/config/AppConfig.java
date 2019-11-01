95442fd7b5e00ea790191259a53342e50e639337
陈其苗 <chenqimiao1994@126.com>
2019-11-02 02:01:31 +0800

    导入IDEA，完成编译，新增一个测试Module[spring-chenqimiao]

--- spring-chenqimiao/src/main/java/com/github/chenqimiao/config/AppConfig.java
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.github.chenqimiao.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Auther: chenqimiao
  * @Date: 2019/11/1 16:34
  * @Description:
  */
-@ComponentScan("com.github.")
+@Configuration
+@ComponentScan("com.github.chenqimiao")
 public class AppConfig {}
