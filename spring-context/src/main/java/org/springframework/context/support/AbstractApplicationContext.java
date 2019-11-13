f0034b564c8a9f86f7d221e54d620421b6cddc84
陈其苗 <chenqimiao1994@126.com>
2019-11-13 09:39:48 +0800

    解析bean instantiation相关代码

--- spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
@@ -554,7 +554,7 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 				registerListeners();
 
 				// Instantiate all remaining (non-lazy-init) singletons.
-				//实例化单例Bean
+				//实例化用户自定义（非Spring开天劈地存在的）的单例Bean
 				finishBeanFactoryInitialization(beanFactory);
 
 				// Last step: publish corresponding event.
