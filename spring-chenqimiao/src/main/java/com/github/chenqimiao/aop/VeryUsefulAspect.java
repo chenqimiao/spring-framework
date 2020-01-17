0cb81f6cc1d303298e7b0a9f4496faead09601bd
陈其苗 <chenqimiao1994@126.com>
2020-01-17 15:43:41 +0800

    AOP概念理解

--- spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/VeryUsefulAspect.java
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/VeryUsefulAspect.java
@@ -14,21 +14,23 @@ import org.springframework.stereotype.Component;
  * @Description:
  */
 @Component
+//切面，所有的组成
 @Aspect
 @Order(2)
 public class VeryUsefulAspect {
-
+	//切点，包含多个连接点，而连接点指的是可执行程序的点，java中即方法
 	@Pointcut(value = "execution(* com.github.chenqimiao.component..*(..))")
 	public void  veryUsefulPointcut(){
 
 	}
 
-
+	//advice 通知建议
 	@After("veryUsefulPointcut()")
 	public void veryUsefulAdvice() {
 		System.out.println("---------------after veryUsefulAdvice aop---------------");
 	}
-
+	
+	//advice 通知建议
 	@Before("veryUsefulPointcut()")
 	public void veryUsefulAdvice(JoinPoint jp) {
 		System.out.println("---------------before veryUsefulAdvice aop---------------");
