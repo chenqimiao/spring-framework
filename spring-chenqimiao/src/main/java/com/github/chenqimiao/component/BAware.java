7994b3e4a6bb0b5fcd1fb9313d79e01369e82127
陈其苗 <chenqimiao1994@126.com>
2020-01-19 09:52:12 +0800

    XxxAware按照spring标准，应该要继承Aware

--- spring-chenqimiao/src/main/java/com/github/chenqimiao/component/BAware.java
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/component/BAware.java
@@ -1,11 +1,13 @@
 package com.github.chenqimiao.component;
 
+import org.springframework.beans.factory.Aware;
+
 /**
  * @Auther: chenqimiao
  * @Date: 2020/1/5 22:29
  * @Description:
  */
-public interface BAware {
+public interface BAware extends Aware {
 
 
 	void setB(B b);
