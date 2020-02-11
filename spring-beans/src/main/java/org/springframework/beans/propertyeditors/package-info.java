c2ca5c9b3a75544c67776250bf81f2e0a5086c08
陈其苗 <chenqimiao1994@126.com>
2020-02-11 20:35:51 +0800

    PropertyEditor vs Converter vs Formatter 说明

--- spring-beans/src/main/java/org/springframework/beans/propertyeditors/package-info.java
+++ spring-beans/src/main/java/org/springframework/beans/propertyeditors/package-info.java
@@ -12,3 +12,15 @@ package org.springframework.beans.propertyeditors;
 
 import org.springframework.lang.NonNullApi;
 import org.springframework.lang.NonNullFields;
+
+/**
+ *
+ * PropertyEditor vs Converter vs Formatter
+ *
+ * PropertyEditor 是java 定义的bean标准属性编辑器，source type 必须是 String, 使用场景不限制在Spring容器
+ *
+ * Converter 是Spring定义的在Spring容器中使用的转化器，依赖于ConversionService的注册，可以自由定义source 和 target 的类型
+ *
+ * Formatter 是为了满足在典型的客户端环境下（web/desktop应用）的类型转化需求而引入的，支持source <-> target的双向转化，source 类型必须是String.
+ *
+ */
