5e99bf6bebabc0c8a0bbef65254fd63e0b1c4429
陈其苗 <chenqimiao1994@126.com>
2020-02-25 00:32:11 +0800

    scope 实验

---
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/ConfigurationTestApplication.java
@@ -0,0 +1,44 @@
+package com.github.chenqimiao;
+
+import com.github.chenqimiao.component.A;
+import com.github.chenqimiao.component.B;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @Auther: chenqimiao
+ * @Date: 2020/2/24 20:55
+ * @Description:
+ */
+public class ConfigurationTestApplication {
+
+
+	@Bean
+	public A a(){
+		return new A();
+	}
+
+
+	@Bean
+	public B b(){
+		return new B(a());
+	}
+
+
+	static class A{
+
+	}
+	static class B{
+		public A a;
+		public B(A a) {
+			this.a = a;
+		}
+	}
+
+
+	public static void main(String args[]){
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ConfigurationTestApplication.class);
+		System.out.println( "a == b.a ? " + ((ac.getBean("a")) == (ac.getBean("b",B.class).a)));
+	}
+
+}
