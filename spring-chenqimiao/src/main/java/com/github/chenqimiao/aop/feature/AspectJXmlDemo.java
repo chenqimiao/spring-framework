be04ed148b5e6985107451893d5312c3df822f27
Qimiao Chen <chenqimiao1994@126.com>
2020-12-31 16:05:15 +0800

    AspectJ demo

---
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/feature/AspectJXmlDemo.java
@@ -0,0 +1,21 @@
+package com.github.chenqimiao.aop.feature;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Description:
+ * @Author: Qimiao Chen
+ * @Create: 2020-12-31 15:51
+ **/
+public class AspectJXmlDemo {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/aop/aspectj.xml");
+		AspectJXmlDemo aspectJXmlDemo = context.getBean(AspectJXmlDemo.class);
+		aspectJXmlDemo.execute();
+	}
+
+	public void execute(){
+		System.out.println("execute() ...");
+	}
+}
---
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/feature/AspectXmlConfig.java
@@ -0,0 +1,13 @@
+package com.github.chenqimiao.aop.feature;
+
+/**
+ * @Description:
+ * @Author: Qimiao Chen
+ * @Create: 2020-12-31 15:57
+ **/
+public class AspectXmlConfig {
+
+	public void before(){
+		System.out.println("@Before advice ...");
+	}
+}
