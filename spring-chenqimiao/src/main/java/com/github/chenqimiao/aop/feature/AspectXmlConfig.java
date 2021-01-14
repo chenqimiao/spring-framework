0dd2b18f32c263951f20f4ada3a1447b2548f057
Qimiao Chen <chenqimiao1994@126.com>
2021-01-14 15:38:39 +0800

    Xml配置Advice

--- spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/feature/AspectXmlConfig.java
+++ spring-chenqimiao/src/main/java/com/github/chenqimiao/aop/feature/AspectXmlConfig.java
@@ -1,7 +1,12 @@
 package com.github.chenqimiao.aop.feature;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
+
+import java.util.Random;
 
 /**
  * @Description:
@@ -15,7 +20,24 @@ public class AspectXmlConfig {
 	}
 
 	public void around(ProceedingJoinPoint joinPoint) throws Throwable{
+		Random random = new Random();
+		if (random.nextBoolean()){
+			throw new RuntimeException("On purpose.");
+		}
 		System.out.println("@Around advice ...");
 		joinPoint.proceed();
+	}
+
+
+	public void after() throws Throwable{
+		System.out.println("@After advice ...");
+	}
+
+	public void afterReturning() throws Throwable{
+		System.out.println("@AfterReturning advice ...");
+	}
+
+	public void afterThrowing() throws Throwable{
+		System.out.println("@AfterThrowing advice ...");
 	}
 }
